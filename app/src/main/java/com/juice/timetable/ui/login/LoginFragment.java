7b2a7c7ee7b1432de4a041c2e6bf37ee19f4d5ca
Oscar-x <2796769363@qq.com>
2020-04-30 22:21:01 +0800

    做了 UI的界面

--- app/src/main/java/com/juice/timetable/ui/login/LoginFragment.java
+++ app/src/main/java/com/juice/timetable/ui/login/LoginFragment.java
@@ -4,32 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.juice.timetable.R;
+import com.juice.timetable.databinding.FragmentSlideshowBinding;
 
 public class LoginFragment extends Fragment {
-
+    private FragmentSlideshowBinding binding;
     private LoginViewModel loginViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        loginViewModel =
-                ViewModelProviders.of(this).get(LoginViewModel.class);
+        loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+        binding = FragmentSlideshowBinding.inflate(getLayoutInflater());//binding初始化
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        loginViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
+
     }
+
 }
