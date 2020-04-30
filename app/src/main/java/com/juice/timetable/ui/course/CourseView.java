7be61507c22c3ce1812bb3ecde9e506733230f49
Aaron <1546848781@qq.com>
2020-04-30 13:04:05 +0800

    add week and node in Course enable dataBinding dataBinding 用viewBinding的方式初始化也没问题

---
+++ app/src/main/java/com/juice/timetable/ui/course/CourseView.java
@@ -0,0 +1,27 @@
+package com.juice.timetable.ui.course;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * <pre>
+ *     author : Aaron
+ *     time   : 2020/04/29
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class CourseView extends FrameLayout {
+    public CourseView(@NonNull Context context) {
+        super(context);
+    }
+
+    public CourseView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
