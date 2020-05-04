815b6a9bfe1dea185168c5c50a574c98c0e229ce
soreak <1498985670@qq.com>
2020-05-04 14:28:06 +0800

    创建了StuInfo类的Dao

---
+++ app/src/main/java/com/juice/timetable/data/StuInfoRepository.java
@@ -0,0 +1,15 @@
+package com.juice.timetable.data;
+
+/**
+ * <pre>
+ *     author : soreak
+ *     e-mail : sorea1k@163.com
+ *     time   : 2020/05/04
+ *     desc   : nothing
+ *     item   : juice
+ *     version: 1.0
+ * </pre>
+ */
+public class StuInfoRepository {
+
+}
