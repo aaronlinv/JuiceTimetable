7cb22ebaf1f03611d11025a3964082013870f4ce
Aaron <1546848781@qq.com>
2020-05-16 17:29:34 +0800

    教务系统 okHttp修改

--- app/src/androidTest/java/com/juice/timetable/data/http/EduHttpTest.java
+++ app/src/androidTest/java/com/juice/timetable/data/http/EduHttpTest.java
@@ -4,8 +4,10 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.juice.timetable.utils.LogUtils;
 import com.juice.timetable.utils.UserInfoUtils;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -17,40 +19,33 @@ import org.junit.Test;
  * </pre>
  */
 public class EduHttpTest {
-
-
-    @Test
-    public void getCookie() {
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-                    String cookie = EduHttp.getCookie(getID(), getEduPasswd(), getContext());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
-
     public Context getContext() {
         return InstrumentationRegistry.getInstrumentation().getTargetContext();
     }
 
-    public String getUserInfo(String info) {
-        return UserInfoUtils.getINSTANT(getContext()).getProperty(info);
+    /**
+     * 测试模拟登录教务系统
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetCookie() throws Exception {
+        UserInfoUtils instant = UserInfoUtils.getINSTANT(getContext());
+        String cookie = EduHttp.getCookie(instant.getID(), instant.getEduPasswd(), getContext());
+        LogUtils.getInstance().e("获取教务系统Cookie：" + cookie);
     }
 
-    public String getID() {
-        return getUserInfo("id");
-    }
+    /**
+     * 测试模拟登录教务系统
+     * 信息错误情况
+     */
+    @Test
+    public void testGetCookieWrongInfo() {
+        try {
+            String cookie = EduHttp.getCookie("211709111", "testWrongPWD", getContext());
+        } catch (Exception e) {
+            Assert.assertEquals(null, "您输入的教务网用户名或是密码有误", e.getMessage());
+        }
 
-    public String getEduPasswd() {
-        return getUserInfo("eduPasswd");
-    }
-
-    public String getLeavePasswd() {
-        return getUserInfo("leavePasswd");
     }
 }
--- app/src/main/java/com/juice/timetable/data/http/EduHttp.java
+++ app/src/main/java/com/juice/timetable/data/http/EduHttp.java
@@ -15,7 +15,16 @@ import org.apache.commons.httpclient.cookie.CookiePolicy;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+
+import kotlin.Pair;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * <pre>
@@ -26,12 +35,116 @@ import java.io.InputStream;
  * </pre>
  */
 public class EduHttp {
+    private static OkHttpClient client = HttpUtils.getHttpClient();
+
     public static String getCookie(String stuID, String stuPassword, Context context) throws Exception {
+        String firstCookie = firstLogin();
+        Bitmap captcha = getCaptcha(firstCookie);
+        String code = CaptchaUtils.getAllOcr(captcha, context);
+        LogUtils.getInstance().e("自动识别验证结果：" + code);
+
+        String cookies = login(stuID, stuPassword, firstCookie, code);
+        return cookies;
+    }
+
+    /**
+     * 初次登录，获取Cookie
+     *
+     * @return
+     */
+    private static String firstLogin() throws IOException {
+        // 教务网主页url
+        String mainURL = "http://jwb.fdzcxy.com/default.asp";
+
+        Request request = new Request.Builder()
+                .get()
+                .url(mainURL)
+                .build();
+        Response response = client.newCall(request).execute();
+        // cookie持久化
+        List<String> cookies = response.headers("Set-Cookie");
+        LogUtils.getInstance().d("cookie：" + cookies);
+
+        StringBuffer cookieStr = new StringBuffer();
+        for (String cookie : cookies) {
+            cookieStr.append(cookie);
+            cookieStr.append(";");
+        }
+        LogUtils.getInstance().d("Cookie String:" + cookieStr);
+        return cookieStr.toString();
+    }
+
+    /**
+     * 获取验证码
+     *
+     * @return
+     */
+    private static Bitmap getCaptcha(String firstCookie) throws IOException {
+        // 验证码获取url
+        String checkCodeURL = "http://jwb.fdzcxy.com/ValidateCookie.asp";
+        LogUtils.getInstance().d("获取验证码的cookie：" + firstCookie);
+        Request request = new Request.Builder()
+                .addHeader("Cookie", firstCookie)
+                .get()
+                .url(checkCodeURL)
+                .build();
+        Response response = client.newCall(request).execute();
+        InputStream inputStream = response.body().byteStream();
+        return BitmapFactory.decodeStream(inputStream);
+    }
+
+    private static String login(String stuID, String stuPassword, String firstCookie, String code) throws Exception {
+        // 登录入口url
+        String loginURL = "http://jwb.fdzcxy.com/loginchk.asp?id=.447517";
+
+        // okHttp
+        FormBody formBody = new FormBody.Builder()
+                .add("muser", stuID)
+                .add("passWd", stuPassword)
+                .add("code", code)
+                .build();
+        Request request = new Request.Builder()
+                .addHeader("cookie", firstCookie)
+                .post(formBody)
+                .url(loginURL)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        Headers headers = response.headers();
+        for (Pair<? extends String, ? extends String> header : headers) {
+            LogUtils.getInstance().d("header:" + header.toString());
+
+        }
+//        okHttp似乎不会返回302，直接返回登录成功的200
+//        int statusCode = response.code();
+//        LogUtils.getInstance().d("模拟登录状态码：" + statusCode);
+        String result = response.body().string();
+        LogUtils.getInstance().d(result);
+        if (result == null) {
+            throw new Exception("responseBody为空");
+        } else if (result.contains("您输入的用户名或是密码出错")) {
+            throw new Exception("您输入的教务网用户名或是密码有误");
+        } else if (result.contains("您输入的验证码不正确")) {
+            throw new Exception("验证码识别失败，请再尝试一次");
+        } else if (result.contains("您已连续输错密码3次，请过5分钟再尝试")) {
+            throw new Exception("您已连续输错教务网密码3次，请过5分钟再尝试");
+        } else if (!result.contains("网络办公系统")) {
+            LogUtils.getInstance().d("模拟登录失败");
+        }
+
+        StringBuffer cookie = new StringBuffer(firstCookie);
+        // 需要添加Cookie：muser
+        cookie.append("muser=").append(stuID);
+        return cookie.toString();
+    }
+
+    public static String getCookieOld(String stuID, String stuPassword, Context context) throws
+            Exception {
+
         String cookies = "";
         String code = "";
 
-        // 教务网主页url
-        String mainURL = "http://jwb.fdzcxy.com/default.asp";
+
         // 登录入口url
         String loginURL = "http://jwb.fdzcxy.com/loginchk.asp?id=.447517";
         // 验证码获取url
@@ -41,7 +154,7 @@ public class EduHttp {
         // 创建浏览器对象
         HttpClient httpClient = new HttpClient();
         // 访问主页获取ssid
-        GetMethod getMethod = new GetMethod(mainURL);
+        GetMethod getMethod = new GetMethod("mainURL");
 
         //设置HttpClient接收Cookie
         httpClient.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
