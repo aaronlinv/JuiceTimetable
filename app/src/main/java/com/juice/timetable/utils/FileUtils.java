3a597e9607ad43d4252b9644aa32e828656b4f56
Aaron <1546848781@qq.com>
2020-05-16 17:40:23 +0800

    删除写入验证码模块

--- app/src/main/java/com/juice/timetable/utils/FileUtils.java
+++ app/src/main/java/com/juice/timetable/utils/FileUtils.java
@@ -1,11 +1,5 @@
 package com.juice.timetable.utils;
 
-import android.content.Context;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -54,47 +48,5 @@ public class FileUtils {
 
     }
 
-    /**
-     * 获取保存的验证码图
-     * 数据区：checkCode.jpg
-     *
-     * @param context
-     * @return
-     */
-    public static InputStream getFile(Context context) {
-        File file = new File(context.getFilesDir(), "checkCode.jpg");
-        try {
-            return new FileInputStream(file);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 
-    /**
-     * 写入网络获取的验证码
-     * 数据区：checkCode.jpg
-     *
-     * @param context
-     * @param is
-     * @return
-     */
-    public static boolean saveFile(Context context, InputStream is, String fileName) {
-        File file = new File(context.getFilesDir(), fileName + ".jpg");
-        try {
-            FileOutputStream fos = new FileOutputStream(file);
-            byte[] b = new byte[1024];
-            int n;
-            while ((n = is.read(b)) != -1) {
-                fos.write(b, 0, n);
-            }
-            is.close();
-            fos.close();
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-
-    }
 }
