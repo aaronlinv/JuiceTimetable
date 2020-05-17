5eca22eed79e61d374c5669f6da87129c9776ece
Aaron <1546848781@qq.com>
2020-05-17 14:17:11 +0800

    解耦获取未签名单

--- app/src/main/java/com/juice/timetable/ui/unsigned/UnsignedFragment.java
+++ app/src/main/java/com/juice/timetable/ui/unsigned/UnsignedFragment.java
@@ -24,9 +24,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.juice.timetable.R;
+import com.juice.timetable.app.Constant;
 import com.juice.timetable.data.JuiceDatabase;
 import com.juice.timetable.data.bean.ClassNoSignedItem;
+import com.juice.timetable.data.bean.StuInfo;
 import com.juice.timetable.data.dao.ClassNoSignedItemDao;
+import com.juice.timetable.data.dao.StuInfoDao;
 import com.juice.timetable.data.http.LeaveInfo;
 import com.juice.timetable.data.parse.ParseClassNoSignedItem;
 import com.juice.timetable.data.viewmodel.ClassNoSignedItemViewModel;
@@ -102,7 +105,12 @@ public class UnsignedFragment extends Fragment {
                 Message message = new Message();
                 if (isNetworkConnected(requireContext())) {
                     try {
-                        String unsigned = LeaveInfo.getUnsignedList(requireContext());
+                        // 从数据库获取个人用户
+                        JuiceDatabase database = JuiceDatabase.getDatabase(requireContext());
+                        StuInfoDao stuInfoDao = database.getStuInfoDao();
+                        StuInfo stuInfo = stuInfoDao.getStuInfo();
+//
+                        String unsigned = LeaveInfo.getLeave(stuInfo.getStuID().toString(), stuInfo.getLeavePassword(), Constant.URI_UNSIGNED_LIST, requireContext());
                         unsignedList = ParseClassNoSignedItem.getClassUnSigned(unsigned);
                         // 删除数据库
                         classNoSignedItemDao.deleteNoSignedItem();
