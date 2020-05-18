9a2ada94e5df44cbcdfa3628a7259a1a43d8eec6
cjw0816 <394189874@qq.com>
2020-05-18 16:03:27 +0800

    消除warning

--- app/src/main/java/com/juice/timetable/ui/about/AboutFragment.java
+++ app/src/main/java/com/juice/timetable/ui/about/AboutFragment.java
@@ -62,26 +62,23 @@ public class AboutFragment extends Fragment {
      * 调用 joinQQGroup(NP5EqyF94Aiyi5CQoNr4-yZYyv5SynLZ) 即可发起手Q客户端申请加群 冲鸭(957024515)
      *
      * @param key 由官网生成的key
-     * @return 返回true表示呼起手Q成功，返回fals表示呼起失败
      ******************/
-    public boolean joinQQGroup(String key) {
+    private void joinQQGroup(String key) {
         Intent intent = new Intent();
         intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D" + key));
         // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         try {
             startActivity(intent);
-            return true;
         } catch (Exception e) {
             Toast.makeText(requireActivity(), "您还没有安装QQ，请先安装软件", Toast.LENGTH_SHORT).show();
             // 未安装手Q或安装的版本不支持
-            return false;
         }
     }
 
     /*******
      * 去除下划线
      */
-    public class NoUnderlineSpan extends UnderlineSpan {
+    public static class NoUnderlineSpan extends UnderlineSpan {
 
         @Override
         public void updateDrawState(TextPaint ds) {
