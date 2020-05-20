5e88c93b6830e7d9d49cb4fe4255db995b70946c
cjw0816 <394189874@qq.com>
2020-05-20 10:25:31 +0800

    删除一些Toast，更改为SnackBar

--- app/src/main/java/com/juice/timetable/ui/unsigned/UnsignedFragment.java
+++ app/src/main/java/com/juice/timetable/ui/unsigned/UnsignedFragment.java
@@ -2,11 +2,13 @@ package com.juice.timetable.ui.unsigned;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -18,10 +20,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.juice.timetable.R;
 import com.juice.timetable.app.Constant;
 import com.juice.timetable.data.JuiceDatabase;
@@ -159,9 +163,22 @@ public class UnsignedFragment extends Fragment {
                 if ("success".equals(msgStr)) {
                     Utils.showToast(requireActivity(), "未签名单更新成功");
                 } else if ("passwd".equals(msgStr)) {
-                    Utils.showToast(requireActivity(), "未输入请假系统密码");
+                    Snackbar.make(requireView(), "未输入请假系统密码", Snackbar.LENGTH_SHORT)
+                            .setAction("去加密码", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Navigation.findNavController(requireActivity(), R.id.nav_host_fragment).navigate(R.id.nav_login);
+                                }
+                            }).show();
                 } else if ("network".equals(msgStr)) {
-                    Utils.showToast(requireActivity(), "设备未联网");
+                    Snackbar.make(requireView(), "设备未联网", Snackbar.LENGTH_SHORT)
+                            .setAction("去联网", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent intent = new Intent(Settings.ACTION_AIRPLANE_MODE_SETTINGS);
+                                    startActivity(intent);
+                                }
+                            }).show();
                 }
                 swipeRefreshLayout.setRefreshing(false);
             }
