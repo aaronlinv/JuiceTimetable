a0fc09a6a8bc0e69cfcee0449c69ad56d9b8e12d
cjw0816 <394189874@qq.com>
2020-05-24 10:23:11 +0800

    style:去除无用代码

--- app/src/main/java/com/juice/timetable/widget/TodayWidget.java
+++ app/src/main/java/com/juice/timetable/widget/TodayWidget.java
@@ -1,6 +1,5 @@
 package com.juice.timetable.widget;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
@@ -8,38 +7,17 @@ import android.content.Intent;
 import android.widget.RemoteViews;
 
 import com.juice.timetable.R;
-import com.juice.timetable.data.JuiceDatabase;
-import com.juice.timetable.data.bean.OneWeekCourse;
-import com.juice.timetable.data.dao.OneWeekCourseDao;
-import com.juice.timetable.service.ListViewService;
-
-import java.util.List;
 
 public class TodayWidget extends AppWidgetProvider {
-    private RemoteViews mRemoteViews;
     public static final String ITEM_CLICK = "day.TYPE_LIST";
+    private RemoteViews mRemoteViews;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        JuiceDatabase database = JuiceDatabase.getDatabase(context.getApplicationContext());
-        OneWeekCourseDao oneWeekCourseDao = database.getOneWeekCourseDao();
-        List<OneWeekCourse> oneWeekCourse = oneWeekCourseDao.getOneWeekCourse();
         mRemoteViews = new RemoteViews(context.getPackageName(), R.layout.today_widget);
         mRemoteViews.setTextViewText(R.id.widget_week, getWeekday());
         appWidgetManager.updateAppWidget(appWidgetIds, mRemoteViews);
-        int len = oneWeekCourse.size();
-        for (int i = 0; i < len; i++) {
-            OneWeekCourse oneWeekCourse1 = oneWeekCourse.get(i);
-            mRemoteViews = new RemoteViews(context.getPackageName(), R.layout.today_widget);
-            Intent lvIntent = new Intent(context, ListViewService.class);
-            mRemoteViews.setRemoteAdapter(R.id.lv_test, lvIntent);
-            mRemoteViews.setEmptyView(R.id.lv_test, android.R.id.empty);
-            Intent intent = new Intent(ITEM_CLICK);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            mRemoteViews.setPendingIntentTemplate(R.id.lv_test, pendingIntent);
-            appWidgetManager.updateAppWidget(oneWeekCourse1.getOnlyID(), mRemoteViews);
-        }
-        appWidgetManager.updateAppWidget(appWidgetIds, mRemoteViews);
+
     }
 
     public String getWeekday() {
@@ -84,25 +62,7 @@ public class TodayWidget extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
-        JuiceDatabase database = JuiceDatabase.getDatabase(context.getApplicationContext());
-        OneWeekCourseDao oneWeekCourseDao = database.getOneWeekCourseDao();
-        List<OneWeekCourse> oneWeekCourse = oneWeekCourseDao.getOneWeekCourse();
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
         mRemoteViews = new RemoteViews(context.getPackageName(), R.layout.today_widget);
         mRemoteViews.setTextViewText(R.id.widget_week, getWeekday());
-        int len = oneWeekCourse.size();
-        if (intent.getAction().equals(ITEM_CLICK)) {
-            for (int i = 0; i < len; i++) {
-                OneWeekCourse oneWeekCourse1 = oneWeekCourse.get(i);
-                mRemoteViews = new RemoteViews(context.getPackageName(), R.layout.today_widget);
-                Intent lvIntent = new Intent(context, ListViewService.class);
-                mRemoteViews.setRemoteAdapter(R.id.lv_test, lvIntent);
-                mRemoteViews.setEmptyView(R.id.lv_test, android.R.id.empty);
-                Intent intent1 = new Intent(ITEM_CLICK);
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 1, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
-                mRemoteViews.setPendingIntentTemplate(R.id.lv_test, pendingIntent);
-                appWidgetManager.updateAppWidget(oneWeekCourse1.getOnlyID(), mRemoteViews);
-            }
-        }
     }
 }
