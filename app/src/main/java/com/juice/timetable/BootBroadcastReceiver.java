c9d23e73dcae8ed7f6e3c2d38cb69f6fb33d0d37
cjw0816 <394189874@qq.com>
2020-05-27 22:15:07 +0800

    fix:修复BroadcastReceiver的warning

--- app/src/main/java/com/juice/timetable/BootBroadcastReceiver.java
+++ app/src/main/java/com/juice/timetable/BootBroadcastReceiver.java
@@ -1,10 +1,12 @@
 package com.juice.timetable;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 public class BootBroadcastReceiver extends BroadcastReceiver {
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent thisIntent = new Intent(context, MainActivity.class);
