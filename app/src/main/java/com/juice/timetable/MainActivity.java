c55e6510b347c3e192d8a050521ab34a3393df63
cjw0816 <394189874@qq.com>
2020-06-04 17:48:01 +0800

    fix:导航栏颜色在原生上正常

--- app/src/main/java/com/juice/timetable/MainActivity.java
+++ app/src/main/java/com/juice/timetable/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import androidx.appcompat.widget.Toolbar;
@@ -46,13 +47,27 @@ public class MainActivity extends BaseActivity {
         /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }*/
+        /**
+         * 通知栏透明
+         */
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             Window window = getWindow();
             window.getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
             window.setStatusBarColor(Color.TRANSPARENT);
         }
+        /**
+         * 导航栏透明
+         */
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            // 状态栏（以上几行代码必须，参考setStatusBarColor|setNavigationBarColor方法源码）
+            window.setNavigationBarColor(Color.TRANSPARENT);
             getWindow().setNavigationBarColor(Color.parseColor("#00000000"));
         }
         setContentView(R.layout.activity_main);
