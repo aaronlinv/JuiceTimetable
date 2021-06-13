10c16314e8bbc753d6db204d0279ad24f12ae36f
Auxim10 <2197118363@qq.com>
2021-06-13 12:54:23 +0800

    fix: 解决成绩没有评测导致网页结构变化发生闪退

--- app/src/main/java/com/juice/timetable/data/parse/ParseGrade.java
+++ app/src/main/java/com/juice/timetable/data/parse/ParseGrade.java
@@ -12,9 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ParseGrade {
-    // TODO
-    //出成绩的时候不能查询、会返回一个空列表，写一个对话框，让他们去微信评测
-
     //爬取成绩，一个是统考成绩爬虫，一个是综合成绩的爬虫
 
     public static List<UniGrade> parseUniGrade(String parseStr) {
@@ -51,27 +48,29 @@ public class ParseGrade {
         //解析综合成绩网页源码
         Document doc = Jsoup.parse(parseStr);
         //爬虫
-        Elements rootselect = doc.select("body > table > tbody > tr:nth-child(2) > td > table:nth-child(4) > tbody > tr");
 
-        for (Element ele : rootselect) {
-            //然后获得标签里面具体的内容
-            Elements all = ele.select("td");
-            Element yearall = all.get(0); //学年
-            String year = yearall.text();
-            if (year.equals("选课 时间")) continue;
-            Element courseall = all.get(1);  // 课程
-            String course = courseall.text();
-            Element gradeall = all.get(3);  //成绩
-            String grade = gradeall.text();
+        //如果存在成绩评测，就直接返回空列表
+        if(!doc.toString().contains("评测")){
+            Elements rootselect = doc.select("body > table > tbody > tr:nth-child(2) > td > table:nth-child(4) > tbody > tr");
 
-            SynGrade synGrade = new SynGrade();
-            synGrade.setCouName(course);
-            synGrade.setCouGrade(grade);
-            synGrade.setCouYear(year);
-            synGradeArrayList.add(synGrade);
+            for (Element ele : rootselect) {
+                //然后获得标签里面具体的内容
+                Elements all = ele.select("td");
+                Element yearall = all.get(0); //学年
+                String year = yearall.text();
+                if (year.equals("选课 时间")) continue;
+                Element courseall = all.get(1);  // 课程
+                String course = courseall.text();
+                Element gradeall = all.get(3);  //成绩
+                String grade = gradeall.text();
+
+                SynGrade synGrade = new SynGrade();
+                synGrade.setCouName(course);
+                synGrade.setCouGrade(grade);
+                synGrade.setCouYear(year);
+                synGradeArrayList.add(synGrade);
+            }
         }
         return synGradeArrayList;
     }
-
-
 }
